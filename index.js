85150398d61b1fa98c219e0f4f487b5362f23acb
Yana Ustsinovich <yustinovich@icloud.com>
2019-09-11 11:44:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function driversWithRevenueOver(drivers, revenue) {
 
 function driverNamesWithRevenueOver() {
   driverNamesWithOver = drivers.filter(driver => driver.revenue > revenue);
-
+  newArr = [];
+  newArr.push(driverNamesWithOver.name);
   return driverNamesWithOver;
 }
