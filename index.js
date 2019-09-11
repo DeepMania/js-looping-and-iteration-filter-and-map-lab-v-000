e6052a29fb162f464a6211aff166e5ac89987792
Yana Ustsinovich <yustinovich@icloud.com>
2019-09-11 11:20:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers, revenue) {
-
+  driversWithOver = [];
   driversWithOver.push(drivers.revenue > revenue)
   return driversWithOver;
 }
