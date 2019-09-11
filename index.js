80aeff3266ebaa9d86873ab3d1c0c12abc5b80b4
Yana Ustsinovich <yustinovich@icloud.com>
2019-09-11 11:36:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers, revenue) {
-  
+
   driversWithOver = drivers.filter(driver => driver.revenue > revenue);
 
   return driversWithOver;
 }
 
 function driverNamesWithRevenueOver() {
-  driverNamesWithOver =
+  driverNamesWithOver = drivers.filter(driver => driver.revenue > revenue).map(driver => return driver.name);
+  return driverNamesWithOver;
 }
